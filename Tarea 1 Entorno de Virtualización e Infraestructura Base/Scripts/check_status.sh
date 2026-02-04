f882f2e8f6149601d2d24e89b45a839b08931247
Alberto Torres <torreschaparroalberto91@gmail.com>
2026-02-03 22:51:21 -0700

    Se añadieron comentarios explicativos al script de diagnostico de linux

--- Tarea 1 Entorno de Virtualización e Infraestructura Base/Scripts/check_status.sh
+++ Tarea 1 Entorno de Virtualización e Infraestructura Base/Scripts/check_status.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-echo "========================================"
-echo "INFORMACION DEL SERVIDOR"
-echo "========================================"
-echo "Nombre:           $(hostname)"
-echo "S.O:              $(grep PRETTY_NAME /etc/os-release | cut -d'"' -f2)"
-echo "Direccion IPv4:   $(hostname -I | awk '{print $2}')"
-echo "Espacio en disco: $(df -h / | awk 'NR==2 {print $4 " libres de " $2}')"
+# Script de Diagnostico Basico para linux
+# Proposito: Validar identidad, version y almacenamiento del nodo
+
+echo "============================="
+echo "     DIAGNOSTICO DE LINUX"
+echo "============================="
+
+# 1. Identificacion del Nodo
+# Muestra el nombre del equipo para confirmar en que Nodo estamos
+echo "Nombre: $(hostname)"
+
+# 2. Version del sistema operativo
+# Busca la linea "PRETTY_NAME" en la info del sistema
+echo "S.O: $(grep PRETTY_NAME /etc/os-release | cut -d '"' -f2)"
+
+# 3. Direccion IP (Red interna)
+# host name -I de todas las IPs. Usamos awk '{print $2}' para tomar la segunda IP
+# ya que la primera suele ser la NAT y la segunda es la red interna (en este caso)
+echo "Direccion IPv4: $(hostname -I | awk '{print $2}')"
+
+# 4. Revisa la particion Raiz, salta el encabezado y muestra solo el espacio libre y el total
+echo "Espacio de disco: $(df -h / | awk 'NR==2 {print $4 " libres de " $2}')"
