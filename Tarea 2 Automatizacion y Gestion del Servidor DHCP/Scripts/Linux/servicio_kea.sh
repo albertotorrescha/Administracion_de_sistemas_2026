e7c54e6e91045551e4623b37fab252310ff71ccd
Alberto Torres <torreschaparroalberto91@gmail.com>
2026-02-07 00:03:25 -0700

    Implementacion de la instalacion desatendida del servicio kea DHCP

--- Tarea 2 Automatizacion y Gestion del Servidor DHCP/Scripts/Linux/servicio_kea.sh
+++ Tarea 2 Automatizacion y Gestion del Servidor DHCP/Scripts/Linux/servicio_kea.sh
@@ -2,8 +2,7 @@
 # ============================================================
 # Tarea 2:  Automatización y Gestión del Servidor DHCP
 #   Autor:  Alberto Torres Chaparro
-# Parte 1:  Verificar si el software de servidor DHCP esta 
-#           instalado
+# Parte 2:  Instalar el servicio DHCP
 # ============================================================
 
 echo "Comprobando servicio..."
@@ -19,4 +18,25 @@ if rpm -q kea &> /dev/null; then
     fi
 else
     echo "El servicio Kea DHCP NO está instalado en este servidor."
+    # --- Actualizacion de la Parte 2
+    echo "Iniciando instalación..."
+    # Instalando el servicio
+    dnf install -y kea &> /dev/null
+    
+    # Verificamos si la instalación fue exitosa
+    if rpm -q kea &> /dev/null; then
+        echo "Instalación completada con éxito."
+    else
+        echo "Error: No se pudo instalar Kea."
+        exit 1
+    fi
+fi
+echo ">>> Asegurando la ejecución del servicio..."
+systemctl enable --now kea-dhcp4 &> /dev/null
+
+# Verificación final
+if systemctl is-active --quiet kea-dhcp4; then
+    echo "Estado Final: ACTIVO"
+else
+    echo "Estado Final: FALLO AL INICIAR"
 fi
